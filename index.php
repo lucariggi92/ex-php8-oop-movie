496da977bfce69d524acf42b72f635507cf13cca
lucariggi92 <riggiluca92@gmail.com>
2026-04-09 13:30:05 +0200

    feat: class Movie

--- index.php
+++ index.php
@@ -1,7 +1,46 @@
 <?php
 
+class Movie {
+    public $titolo;
+    public $cover;
+    public $regista;
+    public $anno_di_uscita;
+    public $durata;
+    public $genere;
+
+    public function __construct($titolo, $cover, $regista, $anno_di_uscita, $durata, $genere){
+    $this->titolo = $titolo;
+     $this->cover = $cover;
+        $this->regista= $regista;
+         $this->anno_di_uscita= $anno_di_uscita;
+        $this->durata = $durata;
+         $this->genere = $genere;
+
+    }
+
+        public function getMovie() {
+            echo $this->titolo ."<br>";
+            echo $this->cover ."<br>";
+            echo $this->regista ."<br>";
+            echo $this->anno_di_uscita ."<br>";
+            echo $this->durata ."<br>";
+            echo $this->genere ."<br>";
 
 
+        }
+
+}
+
+
+
+
+
+$film1 = new Movie("inception", "cover1.jpg", "Nolan", 2010, 148, "Fantascienza");
+
+$film2 = new Movie("Gladiatore", "cover2.jpg", "Scott", 2000, 120, "Storico");
+
+$film1->getMovie();
+$film2->getMovie();
 
 
 ?>
