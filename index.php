4e24285ffd6a5fb47aeb91a426508e27a94a2c52
lucariggi92 <riggiluca92@gmail.com>
2026-04-09 19:20:12 +0200

    feat: header and footer

--- index.php
+++ index.php
@@ -1,86 +1,5 @@
 <?php
-trait Disponibilità{
-    public $disponibilità;
-
-    public function GetDisponibilità(){
-         echo $this->disponibilità;
-    }
-
-}
-
-class Movie {
-    use Disponibilità;
-
-
-    public $titolo;
-    public $cover;
-    public $regista;
-    public $anno_di_uscita;
-    public $durata;
-    public $genere;
-
-    public function __construct($titolo, $cover, $regista, $anno_di_uscita, $durata, $genere, $disponibilità){
-    $this->titolo = $titolo;
-     $this->cover = $cover;
-        $this->regista= $regista;
-         $this->anno_di_uscita= $anno_di_uscita;
-        $this->durata = $durata;
-         $this->genere = $genere;
-         $this->disponibilità = $disponibilità;
-
-    }
-
-        public function getMovie() {
-            echo $this->titolo ."<br>";
-            echo $this->cover ."<br>";
-            echo $this->regista ."<br>";
-            echo $this->anno_di_uscita ."<br>";
-            echo $this->durata ."<br>";
-            foreach($this->genere as $elemento){
-                $elemento->getGenre();                        
-            };
-             echo $this->GetDisponibilità();   
-
-
-        }
-
-    
-    
-
-}
-
-class Genre {
-    public $nome;
-    public $eta_minima;
-    public $descrizione;
-
-    public function __construct($nome, $eta_minima, $descrizione){
-        $this->nome = $nome;
-        $this->eta_minima = $eta_minima;
-        $this->descrizione =$descrizione;
-    }
-
-    public function getGenre(){
-        echo $this->nome ."<br>";
-            echo $this->eta_minima ."<br>";
-            echo $this->descrizione ."<br>";
-          
-    }
-}
-
-
-$genere1 = new Genre("Fantascienza", 12, "Film di fantascienza");
-
-$genere2 = new Genre("Storico", 13, "Film Storico");
-
-$genere3 = new Genre("Avventura", 14, "Film d'avventura");
-$genere4 = new Genre("Guerra", 15, "Film di guerra");
-
-
-$film1 = new Movie("inception", "cover1.jpg", "Nolan", 2010, 148, [$genere1, $genere3], "On Demand");
-
-$film2 = new Movie("Gladiatore", "cover2.jpg", "Scott", 2000, 120, [$genere2, $genere4], "Al Cinema");
-
+require_once "./db.php";
 
 
 $film1->getMovie();
@@ -88,9 +7,6 @@ $film2->getMovie();
 
 var_dump($film1);
 var_dump($film2);
-
-
-
 ?>
 
 
@@ -99,15 +15,61 @@ var_dump($film2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie</title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-   
-    <div class= "container-fluid py-3">
-        <h1>Movie</h1>
+
+    <!-- HEADER ------------------------------------------->
+    
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Film</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Serie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nuovi e popolari</a>
+            </li>
+     </ul>
     </div>
+
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<!-- FOOTER -->
+<footer class="bg-dark text-secondary py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <p class="mb-1 text-white">MovieCatalog</p>
+                <small>
+                    &copy; <?php echo date("Y"); ?> - Catalogo Film. 
+                    Tutti i dati sono gestiti tramite <code>db.php</code>
+                </small>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
